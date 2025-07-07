7e7c112f3bfd397a0978c750a572eed0b504b6dc
Nucleus <nucleus-ffm@posteo.de>
2025-07-07 11:53:27 +0200

    connect UI to controller and fix config parsing

--- control/actor.py
+++ control/actor.py
@@ -1,4 +1,3 @@
-from typing import Any
 from control.definitions import ActorType, ActionType
 
 class Actor:
--- gui/test_definition_file_explorer.py
+++ gui/test_definition_file_explorer.py
@@ -1,25 +1,23 @@
 from PySide6.QtWidgets import QFileDialog
+
+from control.controller import Controller
 from control.test_definition_parsing import parse_csv
 
-def open_file_dialog(self):
+def open_file_dialog(self, controller:Controller):
     """
     Show a file dialog to select a sequence file
     """
     # Open a file dialog and get the selected file path
     file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "*.csv")
     if file_path:
-        # @TODO parse the CSV and create stuff from that
-        result = parse_csv(file_path)
-        print(result)
         self.label_sequence_file_name.setText(f"Selected file: {file_path}")
+        controller.load_test_definition(file_path)
     else:
         self.label_sequence_file_name.setText("No file selected.")
 
 
-def reload_file(self):
+def reload_file(self, controller:Controller):
     """
     Reload the same filename again
     """
-    # @TODO implement
-    result = parse_csv(self.label_sequence_file_name.text())
-    print(result)
+    controller.load_test_definition(self.label_sequence_file_name.text())
