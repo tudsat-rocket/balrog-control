a84e33f07adf756299c28c3928822085aa74d11a
Nucleus <nucleus-ffm@posteo.de>
2025-09-08 18:34:38 +0200

    remove redundant x-asis values

--- gui/data_plotter.py
+++ gui/data_plotter.py
@@ -27,41 +27,41 @@ def update_plots(self):
     # pressure
     if len(pressure_0_sensor_list) > 0:
         pressure0 = pressure_0_sensor_list[1].copy()
-        self.pressure_curve_0.setData(create_time_list(len(pressure0)), pressure0)
+        self.pressure_curve_0.setData(pressure0)
         del pressure0
     if len(pressure_1_sensor_list) > 0:
         pressure1 = pressure_1_sensor_list[1].copy()
-        self.pressure_curve_1.setData(create_time_list(len(pressure1)), pressure1)
+        self.pressure_curve_1.setData(pressure1)
         del pressure1
     if len(pressure_2_sensor_list) > 0:
         pressure2 = pressure_2_sensor_list[1].copy()
-        self.pressure_curve_2.setData(create_time_list(len(pressure2)), pressure2)
+        self.pressure_curve_2.setData(pressure2)
         del pressure2
 
     if len(differential_pressure_list) > 0 :
         differential_pressure = differential_pressure_list[1].copy()
-        self.differential_pressure_curve.setData(create_time_list(len(differential_pressure)), differential_pressure)
+        self.differential_pressure_curve.setData(differential_pressure)
         del differential_pressure
 
     # temperature
     if len(temperature_engine_sensor_list) > 0:
         temperature_engine = temperature_engine_sensor_list[1].copy()
-        self.thermocouple_engine_curve.setData(create_time_list(len(temperature_engine)), temperature_engine)
+        self.thermocouple_engine_curve.setData(temperature_engine)
         del temperature_engine
     if len(temperature_nitrous_sensor_list) > 0:
         temperature_nitrous = temperature_nitrous_sensor_list[1].copy()
-        self.thermocouple_nitrous_curve.setData(create_time_list(len(temperature_nitrous)), temperature_nitrous)
+        self.thermocouple_nitrous_curve.setData(temperature_nitrous)
         del temperature_nitrous
 
     # load cell
     if len(load_cell_1_sensor_list) > 0:
         # print(load_cell_1_sensor_list[1])
         load_cell1 = load_cell_1_sensor_list[1].copy()
-        self.load_cell_thrust_curve.setData(create_time_list(len(load_cell1)), load_cell1)
+        self.load_cell_thrust_curve.setData(load_cell1)
         del load_cell1
     if len(load_cell_2_sensor_list):
         load_cell2 = load_cell_2_sensor_list[1].copy()
-        self.load_cell_nitrous_curve.setData(create_time_list(len(load_cell2)), load_cell2)
+        self.load_cell_nitrous_curve.setData(load_cell2)
         del load_cell2
 
     view_buffer = 500
