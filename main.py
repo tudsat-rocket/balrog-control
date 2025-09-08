18db229b61d67010cf3d8927bb3333de5950c152
Nucleus <nucleus-ffm@posteo.de>
2025-09-08 18:32:28 +0200

    continuously store sensor data in csv

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from queue import Queue
 from time import sleep
 from threading import Thread, Event
 from control.controller import Controller
+from control.data_handling import DataHandler
 from gui.main_window import NewMainWindow
 from PySide6.QtWidgets import QApplication
 from random import randint
 
-# store connection state
-connected = False
 
+def data_handler(thread_killer, connected_signal):
+    """
+    data handler in thread, store latest data in CSV
+    """
+    handler = DataHandler()
+    connected_signal.wait()
+    while not thread_killer.is_set():
+        # store the latest data in CSV
+        connected_signal.wait()
+        handler.save()
+        sleep(0.1) # @todo
 
 
 if __name__ == "__main__":
@@ -26,6 +36,8 @@ if __name__ == "__main__":
 
     controller = Controller(event_queue, thread_killer, abort_signal, run_signal)
 
+    data_handler = Thread(target=data_handler, args=(thread_killer,connected_signal))
+    data_handler.start()
 
     """
     Start the main UI window
@@ -38,5 +50,5 @@ if __name__ == "__main__":
 
     # join the threads again
     thread_killer.set()
-    #data_handling_thread.join(timeout=5)
+    data_handler.join(timeout=1)
     sys.exit(rc)
