e511f444ef58afe5e389efa6de9b1c7d7a45b26e
Nucleus <nucleus-ffm@posteo.de>
2025-09-08 18:32:28 +0200

    continuously store sensor data in csv

--- control/data_handling.py
+++ control/data_handling.py
@@ -1,4 +1,7 @@
-
+import os
+from datetime import datetime
+import csv
+from shared.shared_lists import *
 
 
 class DataHandler:
@@ -6,11 +9,90 @@ class DataHandler:
     Aggregate and store measurement data 
     """
     def __init__(self):
-        pass 
+        self.writer = self.create_file(self)
+        self.write_header()
+
+    @staticmethod
+    def get_value_or_minus(index, list):
+        if index >= len(list[1]) or index < 0:
+            return [-1, 1]
+        else:
+            return list[1][index]
+
+    @staticmethod
+    def get_time_or_minus(index, list):
+        if index >= len(list[0]) or index < 0:
+            return [-1, 1]
+        else:
+            return list[0][index]
+
+    def get_latest_value_pair(self, list):
+        """
+        get a pair of the latest timestamp and value
+        """
+        return str(self.get_time_or_minus(len(list[0])-1, list)), self.get_value_or_minus(len(list[0])-1, list)
+
+    @staticmethod
+    def create_file(self):
+        """
+        create a new log file
+        """
+        print("create file for log")
+        # create log folder if not exists
+        if not os.path.exists("logs"):
+            os.makedirs("logs")
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        current_log_filename = f"log_{timestamp}.csv"
+        filepath = os.path.join("logs", current_log_filename)
+        log_file = open(filepath, "w", newline="")
+        return csv.writer(log_file)
+
+    def write_header(self):
+        """
+        write the header file for the csv
+        """
+        self.writer.writerow(
+            ["Time pressure 0", "Pressure 0",
+             "Time pressure 1", "Pressure 1",
+             "Time pressure 2", "Pressure 2",
+             "Time temperature Nitrous", "Temperatur Nitrous",
+             "Time Engine", "Temperatur Engine",
+             "Time load cell", "Thrust load cell",
+             "Time Nitrous load cell", "Nitrous load cell",
+             "Time Differential", "Differential Nitrous pressure",
+             "Time N2OMainValve", "N2OValveState",
+             "Time N2OFillValve", "N2OFillValveState",
+             "Time N2OVentValve", "N2OVentValveState",
+             "Time N2PurgeValve", "N2PurgeValveState",
+             "TIme N2PressureValve", "N2PressureValveState"
+             ]
+        )
 
     def save(self):
-        pass 
+        """
+        save a new data point in the csv
+        """
+        self.writer.writerow(
+            [
+                self.get_latest_value_pair(pressure_0_sensor_list),
+                self.get_latest_value_pair(pressure_1_sensor_list),
+                self.get_latest_value_pair(pressure_2_sensor_list),
+
+                self.get_latest_value_pair(temperature_nitrous_sensor_list),
+                self.get_latest_value_pair(temperature_engine_sensor_list),
+
+                self.get_latest_value_pair(load_cell_1_sensor_list),
+                self.get_latest_value_pair(load_cell_2_sensor_list),
+
+                self.get_latest_value_pair(differential_pressure_list),
+
+                self.get_latest_value_pair(n2o_main_valve_sensor_list),
+                self.get_latest_value_pair(n2o_fill_valve_sensor_list),
+                self.get_latest_value_pair(n2o_vent_valve_sensor_list),
+                self.get_latest_value_pair(n2_purge_valve_sensor_list),
+                self.get_latest_value_pair(n2o_main_valve_sensor_list),
+                self.get_latest_value_pair(n2_pressure_valve_sensor_list),
+        ])
 
     def load(self):
-        pass 
-
+        raise NotImplemented
