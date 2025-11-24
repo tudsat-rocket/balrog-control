fd69fefa9a9697b8ac5d10820c093e1f0cd3db70
Nucleus <nucleus-ffm@posteo.de>
2025-11-24 16:21:13 +0100

    Refactor the ui_updater and follow linter suggestions to improve code quality.

--- gui/ui_updater.py
+++ gui/ui_updater.py
@@ -1,44 +1,65 @@
+"""UI update function.
+
+This module provides methods to update ui elements
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from gui.main_window import NewMainWindow
+
+import logging
 import queue
-from control.definitions import State
-from idlelib.sidebar import EndLineDelegator
+from queue import Queue
 
 from PySide6.QtWidgets import QDialog, QLabel, QVBoxLayout, QDialogButtonBox
-
+from control.definitions import State
 from control.definitions import EventType
-from shared.shared_lists import *
+
+from shared.shared_lists import (
+    n2o_main_valve_sensor_list,
+    n2o_fill_valve_sensor_list,
+    n2o_vent_valve_sensor_list,
+    n2_purge_valve_sensor_list,
+    n2_pressure_valve_sensor_list,
+)
+
+logger = logging.getLogger(__name__)
 
 
-def read_events_values_from_queue(self):
-    """
-    consume the newest values from the sensor queues
-    """
+def read_events_values_from_queue(self: NewMainWindow) -> None:
+    """Consume the newest values from the sensor queues."""
     # get new pressure value from queue
     self.eventdata.append()
 
 
-def update_ui(self):
-    """
-    Update the plots with new sensor values
-    """
-
+def _get_event_from_queue(event_queue: Queue) -> dict | None:
+    """Get the event from the queue."""
     try:
         # set waiting to false
-        event = self.event_queue.get(False)
-        print(f"received event {event}")
+        event: dict = event_queue.get(False)
+        logger.debug("received event %event", event)
     except queue.Empty:
         # queue is empty, Nothing to do.
+        return None
+
+
+def update_ui(self: NewMainWindow) -> None:  # noqa: C901
+    """Update the plots with new sensor values."""
+    event: dict = _get_event_from_queue(self.event_queue)
+    if event is None:
         return
 
     match event["type"]:
         case EventType.CONNECTION_STATUS_UPDATE:
             update_connection_state(self, event)
-        # case EventType.VALVE_STATUS_UPDATE:
-        #     update_valve_state(self, event)
         case EventType.SEQUENCE_STARTED:
-            update_sequence_state(self, False)
+            update_sequence_state(self, enabled=False)
             clear_data_cache(self)
         case EventType.SEQUENCE_STOPPED:
-            update_sequence_state(self, True)
+            update_sequence_state(self, enabled=True)
         case EventType.SEQUENCE_ERROR:
             show_sequence_error(self, event)
         case EventType.INFO_EVENT:
@@ -53,7 +74,12 @@ def update_ui(self):
             reset_plots(self)
 
 
-def update_valve_states(self):
+def update_valve_states(self: NewMainWindow) -> None:
+    """Update the state of every valve.
+
+    This updates the text in the UI where the user can see
+    the current position of the valve.
+    """
     if len(n2o_main_valve_sensor_list[1]) > 0:
         state = n2o_main_valve_sensor_list[1][-1]
         self.label_valve_status_n2o_main_state.setText(str(state))
@@ -71,10 +97,8 @@ def update_valve_states(self):
         self.label_valve_status_n2_pressure_state.setText(str(state))
 
 
-def update_connection_state(self, connection_event):
-    """
-    update the labels to display the current connection status
-    """
+def update_connection_state(self: NewMainWindow, connection_event: dict) -> None:
+    """Update the labels to display the current connection status."""
     self.label_status_connection_state.setText(connection_event["status"])
     self.label_status_hostname_state.setText(connection_event["hostname"])
     self.label_status_port_state.setText(str(connection_event["port"]))
@@ -84,17 +108,14 @@ def update_connection_state(self, connection_event):
         self.button_connect.setText("Connect")
 
 
-def update_sequence_state(self, enabled: bool):
-    """
-    enable and disable the buttons to start a sequence or do abort a sequence
-    """
+def update_sequence_state(self: NewMainWindow, *, enabled: bool) -> None:
+    """Enable and disable the buttons to start a sequence or do abort a sequence."""
     # disable/enable start sequence button
     self.button_start_sequence.setEnabled(enabled)
     self.button_connect.setEnabled(enabled)
 
     self.button_selfcheck.setEnabled(enabled)
     self.button_test_horn.setEnabled(enabled)
-    # self.button_test_light.setEnabled(enabled)
 
     self.button_open_sequence.setEnabled(enabled)
     self.button_reload_sequence.setEnabled(enabled)
@@ -102,20 +123,10 @@ def update_sequence_state(self, enabled: bool):
     self.button_abort_sequence.setEnabled(not enabled)
 
 
-def update_valve_state(self, event):
-    print(event)
-    match event["valve"]:
-        case "main":
-            self.label_valve_status_n20_main_state.setText(str(event["state"]))
-        case "vent":
-            self.label_valve_status_n20_vent_state.setText(str(event["state"]))
-        case "fill":
-            self.label_valve_status_n20_fill_state.setText(str(event["state"]))
+def show_info_event(self: NewMainWindow, info_event: dict) -> None:
+    """Show a dialog with an error message.
 
-
-def show_info_event(self, info_event):
-    """
-    Show a dialog with an error message
+    If something went wrong, we can show the user a dialog with the error message.
     """
     # @TODO(Nucleus): redesign with pyside designer
     dlg = QDialog(self)
@@ -127,29 +138,28 @@ def show_info_event(self, info_event):
     dlg.exec()
 
 
-def show_confirmation_event(self, confirmation_event):
-    """
-    Show a dialog with an confirmation request
+def show_confirmation_event(self: NewMainWindow, confirmation_event: dict) -> None:
+    """Show a dialog with a confirmation request.
+
+    This allows to have state transition a confirmation to change the state.
     """
     dlg = QDialog(self)
     dlg.setWindowTitle(confirmation_event["title"])
     message = QLabel(confirmation_event["message"])
-    buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-    buttonBox.accepted.connect(confirmation_event["confirm"])
-    buttonBox.accepted.connect(dlg.accept)
-    buttonBox.rejected.connect(confirmation_event["cancel"])
-    buttonBox.rejected.connect(dlg.reject)
+    button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+    button_box.accepted.connect(confirmation_event["confirm"])
+    button_box.accepted.connect(dlg.accept)
+    button_box.rejected.connect(confirmation_event["cancel"])
+    button_box.rejected.connect(dlg.reject)
     layout = QVBoxLayout()
     layout.addWidget(message)
-    layout.addWidget(buttonBox)
+    layout.addWidget(button_box)
     dlg.setLayout(layout)
     dlg.exec()
 
 
-def show_sequence_error(self, error_event):
-    """
-    Show a dialog with an error message
-    """
+def show_sequence_error(self: NewMainWindow, error_event: dict) -> None:
+    """Show a dialog with an error message."""
     # @TODO(Nucleus): redesign with pyside designer
     dlg = QDialog(self)
     dlg.setWindowTitle("something went wrong")
@@ -160,9 +170,10 @@ def show_sequence_error(self, error_event):
     dlg.exec()
 
 
-def clear_data_cache(self):
-    """
-    clear the data cache
+def clear_data_cache(self: NewMainWindow) -> None:
+    """Clear the data cache.
+
+    This clears all lists with sensor data.
     """
     self.time_data = []
     self.pressure_1_data = []
@@ -178,38 +189,55 @@ def clear_data_cache(self):
     self.differential_pressure_data = []
 
 
-def update_state(self, event):
+def update_state(self: NewMainWindow, event: dict) -> None:
+    """Update the displayed state in the UI.
+
+    Some actions are restricted to the red state and the user has to
+    enter that state before he can press some buttons.
+    This method does the state transition in the ui.
+    """
     match event["new_state"]:
         case State.GREEN_STATE:
             self.group_state_green.setStyleSheet(
-                "background-color: rgb(143, 240, 164);"
+                "background-color: rgb(143, 240, 164);",
             )
 
             self.group_state_yellow.setStyleSheet(
-                "background-color: rgb(255, 255, 255);"
+                "background-color: rgb(255, 255, 255);",
             )
-            self.group_state_red.setStyleSheet("background-color: rgb(255, 255, 255);")
+            self.group_state_red.setStyleSheet(
+                "background-color: rgb(255, 255, 255);",
+            )
         case State.YELLOW_STATE:
             self.group_state_yellow.setStyleSheet(
-                "background-color: rgb(249, 240, 107);"
+                "background-color: rgb(249, 240, 107);",
             )
 
             self.group_state_green.setStyleSheet(
-                "background-color: rgb(255, 255, 255);"
+                "background-color: rgb(255, 255, 255);",
             )
-            self.group_state_red.setStyleSheet("background-color: rgb(255, 255, 255);")
+            self.group_state_red.setStyleSheet(
+                "background-color: rgb(255, 255, 255);",
+            )
         case State.RED_STATE:
-            self.group_state_red.setStyleSheet("background-color: rgb(255, 160, 160);")
+            self.group_state_red.setStyleSheet(
+                "background-color: rgb(255, 160, 160);",
+            )
 
             self.group_state_yellow.setStyleSheet(
-                "background-color: rgb(255, 255, 255);"
+                "background-color: rgb(255, 255, 255);",
             )
             self.group_state_green.setStyleSheet(
-                "background-color: rgb(255, 255, 255);"
+                "background-color: rgb(255, 255, 255);",
             )
 
 
-def update_arming_state(self, event):
+def update_arming_state(self: NewMainWindow, event: dict) -> None:
+    """Update the state of the arming buttons.
+
+    Some buttons can only be pressed when we are in a armed state.
+    This requires that the arming button is pressed before.
+    """
     match event["new_state"]:
         case True:
             self.button_run_n20_purge_sequence.setEnabled(True)
@@ -227,12 +255,10 @@ def update_arming_state(self, event):
             self.button_start_sequence.setEnabled(False)
 
 
-def reset_plots(self):
-    # self.pressure_curve_0.clear()
-    # self.pressure_curve_2.clear()
-    # self.pressure_curve_1.clear()
-    # self.differential_pressure_curve.clear()
-    # self.thermocouple_engine_curve.clear()
-    # self.thermocouple_nitrous_curve.clear()
+def reset_plots(self: NewMainWindow) -> None:
+    """Reset the plots in the UI.
+
+    This in useful when the sensors are calibrated to remove the old plots.
+    """
     self.load_cell_nitrous_curve.clear()
     self.load_cell_thrust_curve.clear()
